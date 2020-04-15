bb054e858dbbf96f1a76bf2bcc94556cd8dec832
allu <aleksijaakkojuhani@gmail.com>
2020-04-15 16:16:38 +0300

    juukeli

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen';
+StatusBar.setBarStyle('dark-content');
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
+  /* return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  ); */
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen name="Index" component={IndexScreen} options={{ title: 'Blog' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
