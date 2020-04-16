4acb8abaa13d9ef00d364dcf5bc7701e327a2617
allu <aleksijaakkojuhani@gmail.com>
2020-04-16 20:23:46 +0300

    some styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,32 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Context } from '../context/BlogContext';
+import { iosColors } from '../util/globalStyles';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <TouchableOpacity onPress={addBlogPost}>
-        <Text>Add post</Text>
-      </TouchableOpacity>
+      <Button title="Add post" onPress={addBlogPost} />
 
       <FlatList
+        style={styles.list}
         data={state}
         keyExtractor={post => post.title}
         renderItem={({ item }) => {
           return (
-            <Text>
-              {item.title}
-            </Text>
+            <View style={styles.blogPost}>
+              <View style={{justifyContent: 'center'}}>
+                <Text style={styles.blogTitle}>
+                  {item.title}
+                </Text>
+              </View>
+              <TouchableOpacity>
+                <MaterialIcons name="delete-forever" style={styles.deleteIcon} />
+              </TouchableOpacity>
+            </View>
           )
         }}
       />
@@ -27,7 +35,26 @@ const IndexScreen = () => {
 }
 
 const styles = StyleSheet.create({
+  list: {
 
+  },
+  blogPost: {
+    //borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  deleteIcon: {
+    alignSelf: 'center',
+    //color: iosColors.red,
+    fontSize: 35
+  },
+  blogTitle: {
+    //borderWidth: 1,
+    borderColor: 'red',
+    fontSize: 18,
+  },
 });
 
 export default IndexScreen;
